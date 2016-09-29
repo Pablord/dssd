05ed92de8f3125f1cf09d773ff3e6e02ab58924b
Mari Nadile <Mari Nadile>
2016-09-29 15:33:02 -0300

    App google drive

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-	echo ("HOLAAAAA A TODOS");
-?>
+require_once 'google-api-php-client-2.0.3/vendor/autoload.php';
+ 
+session_start();
+require("initClient.php");
+
+if (!$client->getAccessToken() && !isset($_SESSION['token'])) {
+    $authUrl = $client->createAuthUrl();
+    print "<a class='login' href='$authUrl'>Conectar</a>";
+}else{
+	header('Location: http://elpsico.com/distri/listFiles.php');
+}
+
+?> 

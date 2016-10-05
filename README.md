62bc16ee35a869dc93eee338f39535aa067b4fff
Mari Nadile <Mari Nadile>
2016-10-05 14:50:45 -0300

    limpieza

--- README.md
+++
@@ -1,4 +0,0 @@
-dssd-cloud
-==========
-
-A Symfony project created on September 26, 2016, 3:58 am.
--- web/loggedin.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'google-api-php-client-2.0.3/vendor/autoload.php';
- 
-session_start();
-require("initClient.php");
-
-$client->authenticate($_GET['code']);
-$_SESSION['token'] = $client->getAccessToken();
-header('Location: http://elpsico.com/distri/listFiles.php');

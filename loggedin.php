a2c73abf8627ec048c178857463ac288d14e0ed7
Mari Nadile <Mari Nadile>
2016-10-05 14:18:44 -0300

    limpieza

--- loggedin.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
- 
-session_start();
-require("initClient.php");
-
-$client->authenticate($_GET['code']);
-$_SESSION['token'] = $client->getAccessToken();
-header('Location: http://elpsico.com/distri/listFiles.php');

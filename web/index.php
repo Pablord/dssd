5caf076c5ccf927346fea82e862d3a4fabd8cfa2
Mari Nadile <Mari Nadile>
2016-10-05 15:05:21 -0300

    api google

--- web/index.php
+++ web/index.php
@@ -1,25 +1,14 @@
 <?php
+require_once 'google-api-php-client-2.0.3/vendor/autoload.php';
+ 
+session_start();
+require("initClient.php");
 
-require('../vendor/autoload.php');
+if (!$client->getAccessToken() && !isset($_SESSION['token'])) {
+    $authUrl = $client->createAuthUrl();
+    print "<a class='login' href='$authUrl'>Conectar</a>";
+}else{
+	header('Location: http://elpsico.com/distri/listFiles.php');
+}
 
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+?> 
